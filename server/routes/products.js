6445eb12518380b9773c5bf2d0fc6aa36b3718bb
kawawaki <kawawakif@gmail.com>
2023-05-16 10:07:30 +0900

    サーバー構築

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,14 +2,24 @@ const express = require('express')
 const router = express.Router()
 const Product = require('../model/product')
 
-router.get('', function(req, res) {
-    Product.find()
+router.get('', async (req, res) => {
+    const products = await Product.find()
+    res.json(products) 
+    // Product.find({}, function(err, foundProducts) {
+    //     res.json(foundProducts)
+    // })
     // const session = await Product.startSession();
     // Product.startSession().then(foundProducts => {res.json(foundProducts)})
     // Product.startSession(function(err, foundProducts) {
     //     res.json(foundProducts)
     // })
     // res.json({'ok': true})
+})
+
+router.get('/:productId', async (req, res) => {
+    const productId = req.params.productId
+    const product = await Product.findById(productId)
+    res.json(product) 
 })
 
 module.exports = router
